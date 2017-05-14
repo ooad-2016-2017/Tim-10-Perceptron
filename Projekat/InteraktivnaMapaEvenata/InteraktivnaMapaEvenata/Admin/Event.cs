2cf2ded5340c5e4d6ab8c5101ad3f1424754224f
elvircn <elvircrn@gmail.com>
2017-05-14 19:54:49 +0200

    Dodao forme

---
+++ Projekat/InteraktivnaMapaEvenata/InteraktivnaMapaEvenata/Admin/Event.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InteraktivnaMapaEvenata.Admin
+{
+    public class Event
+    {
+        public string Name { get; set; }
+
+        public string Subtitle { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime DateTime { get; set; }
+    }
+}
--- Projekat/InteraktivnaMapaEvenata/InteraktivnaMapaEvenata/App.xaml.cs
+++ Projekat/InteraktivnaMapaEvenata/InteraktivnaMapaEvenata/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using InteraktivnaMapaEvenata.Admin;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -84,7 +85,7 @@ namespace InteraktivnaMapaEvenata
                     // When the navigation stack isn't restored navigate to the first page,
                     // configuring the new page by passing required information as a navigation
                     // parameter
-                    rootFrame.Navigate(typeof(MusterijaMainPage), e.Arguments);
+                    rootFrame.Navigate(typeof(AdminMainPage), e.Arguments);
                 }
                 // Ensure the current window is active
                 Window.Current.Activate();
