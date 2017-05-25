5844472bf7264ac62e6db0550db913c9c8b61492
elvircn <elvircrn@gmail.com>
2017-05-25 22:56:52 +0200

    Reorganizirao UWP modele

---
+++ Projekat/InteraktivnaMapaEvenata/InteraktivnaMapaEvenata.UWPModels/Flag.cs
@@ -0,0 +1,10 @@
+﻿namespace InteraktivnaMapaEvenata.UWP.Models
+{
+    public class Flag
+    {
+        public string Username { get; set; }
+
+        public string Category { get; set; }
+        public string Explanation { get; internal set; }
+    }
+}
