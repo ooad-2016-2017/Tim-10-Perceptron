d73091a6804a09c5fc3f56e538c5f552411c9ce4
Amar.B <amaron_96@live.com>
2017-06-12 12:27:37 +0200

    Serial device stuff

--- ProjekatMVVM/InteraktivnaMapaEvenata.UWPClient/InteraktivnaMapaEvenata/ViewModels/OwnerVM.cs
+++ ProjekatMVVM/InteraktivnaMapaEvenata.UWPClient/InteraktivnaMapaEvenata/ViewModels/OwnerVM.cs
@@ -6,6 +6,10 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using Windows.Devices.Enumeration;
+using Windows.Devices.SerialCommunication;
+using Windows.Storage.Streams;
+using Windows.UI.Popups;
 
 namespace InteraktivnaMapaEvenata.ViewModels
 {
@@ -18,6 +22,8 @@ namespace InteraktivnaMapaEvenata.ViewModels
         {
             OwnerService = ownerService;
         }
+        private SerialDevice device;
+        DataWriter writer = null;
 
         public async void Activate(object parameter)
         {
@@ -28,7 +34,30 @@ namespace InteraktivnaMapaEvenata.ViewModels
                 //ct.ThrowIfCancellationRequested();
                 while (true)
                 {
-                    // Kod za Arduino ide ovdje
+
+
+                    string selector = SerialDevice.GetDeviceSelector("COM4");
+                    DeviceInformationCollection devices = await DeviceInformation.FindAllAsync(selector);
+                    if (devices.Count > 0)
+                    {
+                        DeviceInformation deviceInfo = devices[0];
+                        SerialDevice serialDevice = await SerialDevice.FromIdAsync(deviceInfo.Id);
+                        serialDevice.BaudRate = 9600;
+                        serialDevice.DataBits = 8;
+                        serialDevice.StopBits = SerialStopBitCount.Two;
+                        serialDevice.Parity = SerialParity.None;
+
+                        DataWriter dataWriter = new DataWriter(serialDevice.OutputStream);
+                        dataWriter.WriteString("your message here");
+                        await dataWriter.StoreAsync();
+                        dataWriter.DetachStream();
+                        dataWriter = null;
+                    }
+                    else
+                    {
+                        MessageDialog popup = new MessageDialog("Sorry, no device found.");
+                        await popup.ShowAsync();
+                    }
                 }
             }, tokenSource.Token);
         }
